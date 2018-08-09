06d782068b40ffab3185545370cdbf86782c17ba
toshipp <toshiq2@gmail.com>
2018-08-09 22:43:09 +0900

    swap pgdo/up

--- keyboards/tv44/keymaps/toshipp/keymap.c
+++ keyboards/tv44/keymaps/toshipp/keymap.c
@@ -31,7 +31,7 @@ KC_ESC,    KC_LGUI,KC_LALT,              LOWER,  RAISE,        KC_LBRC,KC_QUOT,K
  * |------`-----`-----`-----`-----`-----`-----`-----`-----`-----`-----`-------|
  * |       |  F1 |  F2 |  F3 |  F4 |  F5 |  F6 |  -  |  ^  |  \  |     | Ins  |
  * |-------`-----`-----`-----`-----`-----`-----`-----`-----`-----`-----`------|
- * |        |  F7 |  F8 |  F9 | F10 | F11 | F12 | Del | HOME| PgUp|  UP | PgDo|
+ * |        |  F7 |  F8 |  F9 | F10 | F11 | F12 | Del | HOME| PgDo|  UP | PgUp|
  * |--------`-----`-----`-----`-----`-----`-----`-----`-----`-----`-----`-----|
  * |      |       |      |             |             |  END | LEFT| Down|RIGHT|
  * `--------------------------------------------------------------------------'
@@ -39,7 +39,7 @@ KC_ESC,    KC_LGUI,KC_LALT,              LOWER,  RAISE,        KC_LBRC,KC_QUOT,K
 [1] = LAYOUT_arrow(
 KC_GRV, KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   _______,
 _______,KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_MINS,KC_EQL, KC_JYEN,_______,KC_INS,
-_______,KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11, KC_F12, KC_DEL, KC_HOME,KC_PGUP,KC_UP,  KC_PGDN,
+_______,KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11, KC_F12, KC_DEL, KC_HOME,KC_PGDN,KC_UP,  KC_PGUP,
 _______,_______,_______,                _______,_______,        KC_END, KC_LEFT,KC_DOWN,KC_RIGHT
 ),
 /* RAISE
@@ -48,7 +48,7 @@ _______,_______,_______,                _______,_______,        KC_END, KC_LEFT,
  * |------`-----`-----`-----`-----`-----`-----`-----`-----`-----`-----`-------|
  * |       |  F1 |  F2 |  F3 |  F4 |  F5 |  F6 |  =  |  ^  |  |  |     | Ins  |
  * |-------`-----`-----`-----`-----`-----`-----`-----`-----`-----`-----`------|
- * |        |  F7 |  F8 |  F9 | F10 | F11 | F12 | Del | HOME| PgUP|  UP | PgDo|
+ * |        |  F7 |  F8 |  F9 | F10 | F11 | F12 | Del | HOME| PgDo|  UP | PgUp|
  * |--------`-----`-----`-----`-----`-----`-----`-----`-----`-----`-----`-----|
  * |      |       |      |             |             |  END | LEFT| Down|RIGHT|
  * `--------------------------------------------------------------------------'
@@ -56,7 +56,7 @@ _______,_______,_______,                _______,_______,        KC_END, KC_LEFT,
 [2] = LAYOUT_arrow(
 KC_GRV, KC_EXLM,KC_AT,  KC_HASH,KC_DLR, KC_PERC,KC_CIRC,KC_AMPR,KC_ASTR,KC_LPRN,KC_0,   _______,
 _______,KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_UNDS,KC_PLUS,KC_SJYN,_______, KC_INS,
-_______,KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11, KC_F12, KC_DEL, KC_HOME,KC_PGUP,KC_UP,  KC_PGDN,
+_______,KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11, KC_F12, KC_DEL, KC_HOME,KC_PGDN,KC_UP,  KC_PGUP,
 _______,_______,_______,                _______,_______,        KC_END, KC_LEFT,KC_DOWN,KC_RIGHT
 ),
 
