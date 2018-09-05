37efa920312b31ea9976b8bf0ca149bb51ef13e3
toshipp <toshiq2@gmail.com>
2018-09-06 00:15:38 +0900

    arrange minus

--- keyboards/tv44/keymaps/toshipp/keymap.c
+++ keyboards/tv44/keymaps/toshipp/keymap.c
@@ -10,7 +10,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Qwerty
  * ,--------------------------------------------------------------------------.
- * | Tab  |  Q  |  W  |  E  |  R  |  T  |  Y  |  U  |  I  |  O  |  P  |  Bksp |
+ * | Tab  |  Q  |  W  |  E  |  R  |  T  |  Y  |  U  |  I  |  O  |  P  |  -    |
  * |------`-----`-----`-----`-----`-----`-----`-----`-----`-----`-----`-------|
  * | Ctrl  |  A  |  S  |  D  |  F  |  G  |  H  |  J  |  K  |  L  |  ;  | Enter|
  * |-------`-----`-----`-----`-----`-----`-----`-----`-----`-----`-----`------|
@@ -20,14 +20,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `--------------------------------------------------------------------------'
  */
 [0] = LAYOUT_arrow(
-KC_TAB, KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_BSPC,
+KC_TAB, KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_MINS,
 KC_LCTL,KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_ENT,
 KC_LSFT,KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,KC_RO,
 KC_ESC,    KC_LGUI,KC_LALT,              LOWER,  RAISE,         KC_LBRC,KC_QUOT,KC_RBRC,KC_NUHS
 ),
 /* LOWER
  * ,--------------------------------------------------------------------------.
- * | Z/H  |  1  |  2  |  3  |  4  |  5  |  6  |  7  |  8  |  9  |  0  |       |
+ * | Z/H  |  1  |  2  |  3  |  4  |  5  |  6  |  7  |  8  |  9  |  0  |  Bksp |
  * |------`-----`-----`-----`-----`-----`-----`-----`-----`-----`-----`-------|
  * |       |  F1 |  F2 |  F3 |  F4 |  F5 |  F6 |  -  |  ^  |  \  |     | Ins  |
  * |-------`-----`-----`-----`-----`-----`-----`-----`-----`-----`-----`------|
@@ -37,14 +37,14 @@ KC_ESC,    KC_LGUI,KC_LALT,              LOWER,  RAISE,         KC_LBRC,KC_QUOT,
  * `--------------------------------------------------------------------------'
  */
 [1] = LAYOUT_arrow(
-KC_GRV, KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   _______,
+KC_GRV, KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_BSPC,
 _______,KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_MINS,KC_EQL, KC_JYEN,_______,KC_INS,
 _______,KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11, KC_F12, KC_DEL, KC_HOME,KC_PGDN,KC_UP,  KC_PGUP,
 MO(4)  ,_______,_______,                _______,_______,        KC_END, KC_LEFT,KC_DOWN,KC_RIGHT
 ),
 /* RAISE
  * ,--------------------------------------------------------------------------.
- * | Z/H  |  !  |  "  |  #  |  $  |  %  |  &  |  '  |  (  |  )  |  0  |       |
+ * | Z/H  |  !  |  "  |  #  |  $  |  %  |  &  |  '  |  (  |  )  |  0  |  Bksp |
  * |------`-----`-----`-----`-----`-----`-----`-----`-----`-----`-----`-------|
  * |       |  F1 |  F2 |  F3 |  F4 |  F5 |  F6 |  =  |  ~  |  |  |     | Ins  |
  * |-------`-----`-----`-----`-----`-----`-----`-----`-----`-----`-----`------|
@@ -54,7 +54,7 @@ MO(4)  ,_______,_______,                _______,_______,        KC_END, KC_LEFT,
  * `--------------------------------------------------------------------------'
  */
 [2] = LAYOUT_arrow(
-KC_GRV, KC_EXLM,KC_AT,  KC_HASH,KC_DLR, KC_PERC,KC_CIRC,KC_AMPR,KC_ASTR,KC_LPRN,KC_0,   _______,
+KC_GRV, KC_EXLM,KC_AT,  KC_HASH,KC_DLR, KC_PERC,KC_CIRC,KC_AMPR,KC_ASTR,KC_LPRN,KC_0,   KC_BSPC,
 _______,KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_UNDS,KC_PLUS,KC_SJYN,_______,KC_INS,
 _______,KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11, KC_F12, KC_DEL, KC_HOME,KC_PGDN,KC_UP,  KC_PGUP,
 MO(4)  ,_______,_______,                _______,_______,        KC_END, KC_LEFT,KC_DOWN,KC_RIGHT
